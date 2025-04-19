4a659c267badbe9717743ba91a172bf0ab9eabf7
Rafael Debein <debeinrafael@gmail.com>
2025-04-19 12:13:49 +0200

    Suppression de taskTypes de taskManager

--- renderer/js/managers/taskManager.js
+++ renderer/js/managers/taskManager.js
@@ -1,37 +1,5 @@
 let tasks = [];
 
-export const taskTypes = [
-	{
-		type: 'Terminal',
-		label: 'Terminal',
-		fields: [
-			{ name: 'projectPath', type: 'text', label: 'Chemin du projet' },
-		]
-	},
-	{
-		type: 'WebPage',
-		label: 'Page Web',
-		fields: [
-			{ name: 'url', type: 'url', label: 'URL de la page' }
-		]
-	},
-	{
-		type: 'VsCode',
-		label: 'VS Code',
-		fields: [
-			{ name: 'projectPath', type: 'text', label: 'Chemin du projet' }
-		]
-	},
-	{
-		type: 'FileExplorer',
-		label: 'Explorateur de fichiers',
-		fields: [
-			{ name: 'projectPath', type: 'text', label: 'Chemin du projet' }
-		]
-	}
-];
-
-
 export function loadTasks() {
 	const data = window.api.lireJson('data/tasks.json');
 	tasks = data;
